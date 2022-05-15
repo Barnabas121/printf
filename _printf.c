7738dfd473dc8fb0e00ea396a19f784e1d91842f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-15 17:23:22 +0300

    betty style updated

--- _printf.c
+++ _printf.c
@@ -76,20 +76,3 @@ int _printf(const char *format, ...)
 	va_end(ap);
 	return (cprint);
 }
-
- */
-int print_string(va_list s)
-{
-	char *my_string;
-	int  i = 0;
-
-	my_string = va_arg(s, char *);
-	if (my_string == NULL)
-		my_string = "(null)";
-	while (my_string[i])
-	{
-		_putchar(my_string[i]);
-		i++;
-	}
-	return (i);
-}
