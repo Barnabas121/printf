19c14519c91faece2473086c06df279f737b0ccd
Barnabas <barnabasabuye998@gmail.com>
2022-05-16 08:24:17 +0300

    updating variable value

--- print_string.c
+++ print_string.c
@@ -15,7 +15,7 @@ int print_string(va_list s)
 
 	my_string = va_arg(s, char *);
 	if (my_string == NULL)
-		my_string = "null";
+		my_string = "\0";
 	while (my_string[i])
 	{
 		_putchar(my_string[i]);
